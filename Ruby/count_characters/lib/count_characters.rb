02940e59bd6086369bccf7818e99eee82412fbcc
Curtis Turk <curtis.c.turk@outlook.com>
2022-10-18 23:39:38 +0100

    exercise example test

--- Ruby/count_characters/lib/count_characters.rb
+++ Ruby/count_characters/lib/count_characters.rb
@@ -1,3 +1,11 @@
 def count_characters(input_string)
-  {}
+  count_object = {}
+  input_string.split('').each{|letter|
+    if(count_object.key?(letter))
+      count_object[letter] += 1
+    else
+      count_object[letter] = 1
+    end
+  }
+  count_object
 end
--- Ruby/count_characters/spec/count_characters_spec.rb
+++ Ruby/count_characters/spec/count_characters_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe "count_chars" do
   it "Returns an object" do
     expect(count_characters("")).to eq({})
   end
+  it "Returns an object with a count" do
+    expect(count_characters("a")).to eq({'a'=> 1})
+  end
+  it "Returns an object with the number of times" do
+    expect(count_characters("aa")).to eq({'a'=> 2})
+  end
+  it "Returns an object with multiple keys" do
+    expect(count_characters("ab")).to eq({'a'=> 1, 'b'=> 1})
+  end
+  it "Returns the correct test given in example" do
+    expect(count_characters("aba")).to eq({'a'=> 2, 'b'=> 1})
+  end
 end
