26876121b558035ce0fb353af0e656dcb917a52a
Curtis Turk <curtis.c.turk@outlook.com>
2022-10-19 17:58:57 +0100

    Completed oddInt

--- JavaScript/oddInt/oddInt.js
+++ JavaScript/oddInt/oddInt.js
@@ -1,3 +1,19 @@
-export default oddInt = (inputArray) => {
+const oddInt = (inputArray) => {
 
+  const numberCounts = inputArray.reduce((allNumbers, number) => {
+    const currentCount = allNumbers[number] || 0
+    return {
+      ...allNumbers,
+      [number]: currentCount + 1
+    };
+  }, {})
+
+  for(let key in numberCounts){
+    if(numberCounts[key] % 2 == 1){
+      return parseInt(key)
+    }
+  }
+  
 }
+
+module.exports = oddInt
--- JavaScript/oddInt/oddInt.test.js
+++ JavaScript/oddInt/oddInt.test.js
@@ -1,7 +1,19 @@
-import oddInt from './oddInt';
+const oddInt = require('./oddInt')
 
 describe("oddInt",()=>{
-  test("returns an array", ()=>{
-    expect(oddInt([]).toEqual([]))
+  test("Returns a number", ()=>{
+    expect(oddInt([0])).toEqual(0)
+  })
+  test("Returns another number", ()=>{
+    expect(oddInt([7])).toEqual(7)
+  })
+  test("Returns the number appearing odd numbers of times", ()=>{
+    expect(oddInt([0,0,1])).toEqual(1)
+  })
+  test("Returns another odd numbered number", ()=>{
+    expect(oddInt([0,1,0,1,0])).toEqual(0)
+  })
+  test("Returns the correct number from a long array", ()=>{
+    expect(oddInt([1,2,2,3,3,3,4,3,3,3,2,2,1])).toEqual(4)
   })
 })
