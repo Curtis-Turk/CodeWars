f0f71dd5a932b3be1c994ea88f9c534065eb6bad
Curtis Turk <curtis.c.turk@outlook.com>
2022-10-20 17:15:49 +0100

    removed line breaks

--- JavaScript/whoLikesIt/whoLikesIt.js
+++ JavaScript/whoLikesIt/whoLikesIt.js
@@ -1,17 +1,11 @@
 const whoLikesIt = (likeArray) => {
   switch (likeArray.length) {
-    case 0:
-      return "no one likes this"
-    case 1:
-      return `${likeArray[0]} likes this`
-    case 2:
-      return `${likeArray[0]} and ${likeArray[1]} like this`
-    case 3:
-      return `${likeArray[0]}, ${likeArray[1]} and ${likeArray[2]} like this`
-    case 4:
-      return `${likeArray[0]}, ${likeArray[1]} and 2 others like this`
-    default:
-      return `${likeArray[0]}, ${likeArray[1]} and ${likeArray.length - 2} others like this`
+    case 0: return "no one likes this"
+    case 1: return `${likeArray[0]} likes this`
+    case 2: return `${likeArray[0]} and ${likeArray[1]} like this`
+    case 3: return `${likeArray[0]}, ${likeArray[1]} and ${likeArray[2]} like this`
+    case 4: return `${likeArray[0]}, ${likeArray[1]} and 2 others like this`
+    default: return `${likeArray[0]}, ${likeArray[1]} and ${likeArray.length - 2} others like this`
   }
 }
 
