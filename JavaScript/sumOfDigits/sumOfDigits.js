8d1b48b92066cb9c3027018c59f88c50519ed90c
Curtis Turk <curtis.c.turk@outlook.com>
2022-10-21 12:38:44 +0100

    Refacotoring solution

--- JavaScript/sumOfDigits/sumOfDigits.js
+++ JavaScript/sumOfDigits/sumOfDigits.js
@@ -1,5 +1,15 @@
 const sumOfDigits = (inputNumber) => {
-  return inputNumber
+  numberArray = inputNumber.toString().split("").map((strNum) => parseInt(strNum))
+  let currentNumber = sumArray(numberArray)
+  if((""+currentNumber).length > 1){
+    return sumOfDigits(currentNumber)
+  }
+  return currentNumber
+}
+
+const sumArray = (numberArray) => {
+  return numberArray.reduce((value,current) =>
+  value + current)  
 }
 
 exports.sumOfDigits = sumOfDigits
--- JavaScript/sumOfDigits/sumOfDigits.test.js
+++ JavaScript/sumOfDigits/sumOfDigits.test.js
@@ -1,5 +1,17 @@
 const { sumOfDigits } = require("./sumOfDigits")
 
-test("It returns the number given if single digit",()=>{
+test("Returns the number given if single digit",()=>{
   expect(sumOfDigits(1)).toEqual(1)
+})
+test("Returns the sum of a 2 digit number",()=>{
+  expect(sumOfDigits(11)).toEqual(2)
+})
+test("Returns the sum of a 3 digit number",()=>{
+  expect(sumOfDigits(111)).toEqual(3)
+})
+test("Returns the sum of a 2 digit number higher than 9",()=>{
+  expect(sumOfDigits(66)).toEqual(3)
+})
+test("Returns the sum of 10 digit number to make a third step",()=>{
+  expect(sumOfDigits(999999999999)).toEqual(9)
 })
