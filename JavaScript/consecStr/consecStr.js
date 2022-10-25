4e11fc76cd5d55897f03141da5fb2f28e675e393
Curtis Turk <curtis.c.turk@outlook.com>
2022-10-25 13:46:29 +0100

    Added another empty case"

--- JavaScript/consecStr/consecStr.js
+++ JavaScript/consecStr/consecStr.js
@@ -1,9 +1,10 @@
 const longestConsec = (strArr, k) => {
   
-  if(strArr.length == 0){
+  if(strArr.length == 0 || k <= 0){
     return ""
   }else
-    return strArr.reduce((a,b)=> {return a.length > b.length ? a : b})
+    return strArr.reduce((a,b)=> 
+    {return a.length > b.length ? a : b})
 }
 
 module.exports = longestConsec
--- JavaScript/consecStr/consecStr.test.js
+++ JavaScript/consecStr/consecStr.test.js
@@ -3,6 +3,9 @@ const longestConsec = require("./consecStr")
 test("return empty string with empty array",()=>{
   expect(longestConsec([],1)).toEqual("")
 })
+test("return empty string with a 0 int",()=>{
+  expect(longestConsec(["string"],0)).toEqual("")
+})
 test("returns the string when given a 1 and a string",()=>{
   expect(longestConsec(["string"],1)).toEqual("string")
 })
