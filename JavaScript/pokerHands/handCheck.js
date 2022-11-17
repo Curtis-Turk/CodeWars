f2637f6479afb9bb98e2a88e8a6528305db96db2
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-17 15:50:13 +0000

    passing tests for all of the main multiple card combinations

--- JavaScript/pokerHands/handCheck.js
+++ JavaScript/pokerHands/handCheck.js
@@ -5,11 +5,31 @@ const checkForMultiples = (cardArray) => {
     uniqueValues.add(card.value);
   });
 
-  if (uniqueValues.size === 4) {
-    return 2;
-  } else if (uniqueValues.size === 3) {
-    return 3;
-  } else if (uniqueValues.size === 2) return 4;
+  switch (uniqueValues.size) {
+    case 4:
+      return "pair";
+    case 3:
+      return check3unique(cardArray, uniqueValues);
+    case 2:
+      return checkfullHouse(cardArray, uniqueValues);
+    default:
+      return "no multiple found";
+  }
+};
+
+const check3unique = (cardArray, uniqueValues) => {
+  for (const cardValue of uniqueValues) {
+    if (cardArray.filter((card) => card.value === cardValue).length === 3)
+      return "triple";
+  }
+  return "2pair";
+};
+const checkfullHouse = (cardArray, uniqueValues) => {
+  for (const cardValue of uniqueValues) {
+    if (cardArray.filter((card) => card.value === cardValue).length === 3)
+      return "fullHouse";
+  }
+  return "four";
 };
 
 module.exports = checkForMultiples;
--- JavaScript/pokerHands/handCheck.test.js
+++ JavaScript/pokerHands/handCheck.test.js
@@ -1,35 +1,59 @@
 const checkForMultiples = require("./handCheck");
 
-test("should return pair with a pair", () => {
-  expect(
-    checkForMultiples([
-      { suit: "heart", value: 2 },
-      { suit: "heart", value: 8 },
-      { suit: "spade", value: 1 },
-      { suit: "club", value: 5 },
-      { suit: "diamond", value: 5 },
-    ])
-  ).toEqual(2);
-});
-test("should return triple with a tiple", () => {
-  expect(
-    checkForMultiples([
-      { suit: "heart", value: 2 },
-      { suit: "heart", value: 8 },
-      { suit: "spade", value: 5 },
-      { suit: "club", value: 5 },
-      { suit: "diamond", value: 5 },
-    ])
-  ).toEqual(3);
-});
-test("should return 4 with a four of a kind", () => {
-  expect(
-    checkForMultiples([
-      { suit: "heart", value: 2 },
-      { suit: "heart", value: 5 },
-      { suit: "spade", value: 5 },
-      { suit: "club", value: 5 },
-      { suit: "diamond", value: 5 },
-    ])
-  ).toEqual(4);
+describe("multiples", () => {
+  test("should return pair with a pair", () => {
+    expect(
+      checkForMultiples([
+        { suit: "heart", value: 2 },
+        { suit: "heart", value: 8 },
+        { suit: "spade", value: 1 },
+        { suit: "club", value: 5 },
+        { suit: "diamond", value: 5 },
+      ])
+    ).toEqual("pair");
+  });
+  test("should return triple with a tiple", () => {
+    expect(
+      checkForMultiples([
+        { suit: "heart", value: 2 },
+        { suit: "heart", value: 8 },
+        { suit: "spade", value: 5 },
+        { suit: "club", value: 5 },
+        { suit: "diamond", value: 5 },
+      ])
+    ).toEqual("triple");
+  });
+  test("should return 4 with a four of a kind", () => {
+    expect(
+      checkForMultiples([
+        { suit: "heart", value: 2 },
+        { suit: "heart", value: 5 },
+        { suit: "spade", value: 5 },
+        { suit: "club", value: 5 },
+        { suit: "diamond", value: 5 },
+      ])
+    ).toEqual("four");
+  });
+  test("should return 2 pair with 2 pairs", () => {
+    expect(
+      checkForMultiples([
+        { suit: "heart", value: 2 },
+        { suit: "heart", value: 2 },
+        { suit: "spade", value: 7 },
+        { suit: "club", value: 5 },
+        { suit: "diamond", value: 5 },
+      ])
+    ).toEqual("2pair");
+  });
+  test("should return fullHouse with a pair and triple", () => {
+    expect(
+      checkForMultiples([
+        { suit: "heart", value: 2 },
+        { suit: "heart", value: 2 },
+        { suit: "spade", value: 5 },
+        { suit: "club", value: 5 },
+        { suit: "diamond", value: 5 },
+      ])
+    ).toEqual("fullHouse");
+  });
 });
