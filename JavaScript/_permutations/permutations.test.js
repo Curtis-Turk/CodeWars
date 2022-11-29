5db8b45c153475e94d28bfcab8c8edaf61b33cf7
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-29 10:20:07 +0000

    passing tests for multiple similar characters

--- JavaScript/_permutations/permutations.test.js
+++ JavaScript/_permutations/permutations.test.js
@@ -3,5 +3,11 @@ const permutations = require("./permuations");
 it("returns a single char in an array", () => {
   expect(permutations("a")).toEqual(["a"]);
 });
+it("returns a 2 char array as a single solution", () => {
+  expect(permutations("aa")).toEqual(["aa"]);
+});
+it("returns a 3 char array as a single solution", () => {
+  expect(permutations("aaa")).toEqual(["aaa"]);
+});
 
 console.log(permutations("a"));
