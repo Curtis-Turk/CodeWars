dbe008151be7022941755a6d051fd3c00cbf01ae
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-26 18:38:40 +0000

    refactored and all tests passing

--- JavaScript/matrixTranspose/matrixTranspose.js
+++ JavaScript/matrixTranspose/matrixTranspose.js
@@ -1,14 +1,13 @@
 function transpose(matrix) {
-  let transposedArray = [];
-
-  matrix.forEach((array) => {
-    console.log(array);
-    array.forEach((digit) => {
-      console.log(digit);
-      transposedArray.push([digit]);
-    });
-  });
-  return transposedArray;
+  let transposed = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    transposed.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      // console.log(transposed);
+      transposed[i].push(matrix[j][i]);
+    }
+  }
+  return transposed;
 }
 
 module.exports = transpose;
--- JavaScript/matrixTranspose/matrixTranspose.test.js
+++ JavaScript/matrixTranspose/matrixTranspose.test.js
@@ -5,3 +5,55 @@ test("should return a digit", () => {
 test("should return a second digit", () => {
   expect(matrixTranspose([[1, 1]])).toEqual([[1], [1]]);
 });
+test("should return a third digit", () => {
+  expect(matrixTranspose([[1, 1, 1]])).toEqual([[1], [1], [1]]);
+});
+
+test("should return a fourth digit", () => {
+  expect(matrixTranspose([[1, 2, 3, 4]])).toEqual([[1], [2], [3], [4]]);
+});
+
+test("should return correctly with another array", () => {
+  expect(matrixTranspose([[1], [2]])).toEqual([[1, 2]]);
+});
+
+test("should return correctly with three arrays", () => {
+  expect(matrixTranspose([[1], [2], [3]])).toEqual([[1, 2, 3]]);
+});
+
+test("should return correctly with mutliple values in multiple arrays", () => {
+  expect(
+    matrixTranspose([
+      [1, 2],
+      [3, 4],
+    ])
+  ).toEqual([
+    [1, 3],
+    [2, 4],
+  ]);
+});
+test("should return correctly with mutliple values in multiple arrays", () => {
+  expect(
+    matrixTranspose([
+      [1, 2, 3],
+      [4, 5, 6],
+    ])
+  ).toEqual([
+    [1, 4],
+    [2, 5],
+    [3, 6],
+  ]);
+});
+
+// test.only("should return correctly with mutliple values in multiple arrays", () => {
+//   expect(
+//     matrixTranspose([
+//       [1, 2],
+//       [3, 4],
+//       [5, 6],
+//     ])
+//   ).toEqual([
+//     [1, 2, 3],
+//     [3, 4, 6],
+//   ]);
+// });
