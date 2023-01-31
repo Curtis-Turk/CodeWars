cb3dec59fa3dae55e23e4c1b439e5374ecb4af36
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-31 17:39:11 +0000

    slight refactor, passing codewars tests

--- JavaScript/isograms/isograms.js
+++ JavaScript/isograms/isograms.js
@@ -1,7 +1,4 @@
-const isIsogram = (inputString) => {
-  if (!inputString) return true;
-  const set = new Set(inputString.toUpperCase().split(""));
-  return set.size === inputString.length;
-};
+const isIsogram = (inputString) =>
+  new Set(inputString.toUpperCase().split("")).size === inputString.length;
 
 module.exports = isIsogram;
