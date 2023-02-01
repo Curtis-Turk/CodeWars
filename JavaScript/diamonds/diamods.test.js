1fc0952e2915a61fe83a566036024fff6b722a73
Curtis Turk <curtis.c.turk@outlook.com>
2023-02-01 16:06:54 +0000

    Solving for a diamond of size five

--- JavaScript/diamonds/diamods.test.js
+++ JavaScript/diamonds/diamods.test.js
@@ -6,3 +6,17 @@ test("should return null if size is 0", () => {
 test("should return diamond of size one with 1", () => {
   expect(diamonds(1)).toEqual("*\n");
 });
+test("should return diamond of size three", () => {
+  expect(diamonds(3)).toEqual(" *\n***\n *\n");
+});
+test("should return diamond of size five", () => {
+  expect(diamonds(5)).toEqual("  *\n ***\n*****\n ***\n  *\n");
+});
+test("should return diamond of size seven", () => {
+  expect(diamonds(7)).toEqual(
+    "   *\n   ***\n  *****\n*******\n  *****\n  ***\n   *\n"
+  );
+});
+test("should return null if size is even", () => {
+  expect(diamonds(2)).toEqual(null);
+});
--- JavaScript/diamonds/diamonds.js
+++ JavaScript/diamonds/diamonds.js
@@ -1,12 +1,22 @@
 function diamonds(size) {
   if (size % 2 === 0 || size <= 0) return null;
 
-  let diamond = "";
-  for (let i = 1; i <= size; i++) {
-    console.log(size, i);
-    diamond += "*\n";
+  let diamond = [];
+  for (let i = 0; i < size; i++) {
+    if (i < size / 2) {
+      diamond.push(" ".repeat(size / 2 - i) + "*".repeat(2 * i + 1) + "\n");
+    } else {
+      diamond.push(
+        " ".repeat(i - size / 2 + 0.5) +
+          "*".repeat(2 * (size - i - 1) + 1) +
+          "\n"
+      );
+    }
   }
-  return diamond;
+
+  console.log(diamond.join(""));
+  return diamond.join("");
 }
 
+diamonds(5);
 module.exports = diamonds;
