95a0222b3c888e2faa429446b2b03b116c6b2c9f
Curtis Turk <curtis.c.turk@outlook.com>
2023-02-01 17:26:13 +0000

    Passing for tests up to 99

--- JavaScript/parseIntReloaded/parseIntReloaded.js
+++ JavaScript/parseIntReloaded/parseIntReloaded.js
@@ -1,29 +1,57 @@
+const ones = {
+  Zero: 0,
+  One: 1,
+  Two: 2,
+  Three: 3,
+  Four: 4,
+  Five: 5,
+  Six: 6,
+  Seven: 7,
+  Eight: 8,
+  Nine: 9,
+};
+
+const teens = {
+  Ten: 10,
+  Eleven: 11,
+  Twelve: 12,
+  Thirteen: 13,
+  Fourteen: 14,
+  Fifteen: 15,
+  Sixteen: 16,
+  Seventeen: 17,
+  Eighteen: 18,
+  Nineteen: 19,
+};
+
+const tens = {
+  Twenty: 20,
+  Thirty: 30,
+  Fourty: 40,
+  Fifty: 50,
+  Sixty: 60,
+  Seventy: 70,
+  Eighty: 80,
+  Ninety: 90,
+};
+
 const parseIntReloaded = (intStr) => {
-  const ones = {
-    One: 1,
-    Two: 2,
-    Three: 3,
-    Four: 4,
-    Five: 5,
-    Six: 6,
-    Seven: 7,
-    Eight: 8,
-    Nine: 9,
-  };
-  const tens = {
-    Ten: 10,
-    Eleven: 11,
-    Twelve: 12,
-    Thirteen: 13,
-    Fourteen: 14,
-    Fifteen: 15,
-    Sixteen: 16,
-    Seventeen: 17,
-    Eighteen: 18,
-    Nineteen: 19,
-  };
-  if (ones[intStr]) return ones[intStr];
-  return tens[intStr];
+  const splitStr = intStr.split("-");
+
+  let numberArr = [];
+
+  for (let i = 0; i < splitStr.length; i++) {
+    const numberString = splitStr[i];
+    if (ones[numberString]) {
+      numberArr.push(ones[numberString]);
+    } else if (teens[numberString]) {
+      numberArr.push(teens[numberString]);
+    } else {
+      numberArr.push(tens[numberString]);
+    }
+  }
+  console.log(numberArr);
+  return numberArr.reduce((a, b) => a + b);
 };
 
 module.exports = parseIntReloaded;
--- JavaScript/parseIntReloaded/parseIntReloaded.test.js
+++ JavaScript/parseIntReloaded/parseIntReloaded.test.js
@@ -20,3 +20,23 @@ test("should return for tens", () => {
   expect(parseIntReloaded("Twelve")).toEqual(12);
   expect(parseIntReloaded("Sixteen")).toEqual(16);
 });
+test("should return for 20", () => {
+  expect(parseIntReloaded("Twenty")).toEqual(20);
+});
+test("should return for tens", () => {
+  expect(parseIntReloaded("Thirty")).toEqual(30);
+  expect(parseIntReloaded("Fourty")).toEqual(40);
+  expect(parseIntReloaded("Fifty")).toEqual(50);
+});
+
+test("should return for Twenty-One", () => {
+  expect(parseIntReloaded("Twenty-One")).toEqual(21);
+});
+
+test("should return for random numbers under 100", () => {
+  expect(parseIntReloaded("Twenty-Three")).toEqual(23);
+  expect(parseIntReloaded("Fourty-Six")).toEqual(46);
+  expect(parseIntReloaded("Seventy-Five")).toEqual(75);
+  expect(parseIntReloaded("Thirty-Two")).toEqual(32);
+  expect(parseIntReloaded("Ninety-Nine")).toEqual(99);
+});
