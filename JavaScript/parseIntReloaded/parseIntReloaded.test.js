c5fa59b10e77272eb316ca0848e651b1d1e769ae
Curtis Turk <curtis.c.turk@outlook.com>
2023-02-02 15:58:04 +0000

    uncommented a test'

--- JavaScript/parseIntReloaded/parseIntReloaded.test.js
+++ JavaScript/parseIntReloaded/parseIntReloaded.test.js
@@ -81,6 +81,6 @@ test("returns for 1000,000", () => {
   expect(parseIntReloaded("one million")).toEqual(1000000);
 });
 
-// test("returns correctly for 100,000", () => {
-//   expect(parseIntReloaded("one hundred thousand")).toEqual(100000);
-// });
+test("returns correctly for 100,000", () => {
+  expect(parseIntReloaded("one hundred thousand")).toEqual(100000);
+});
