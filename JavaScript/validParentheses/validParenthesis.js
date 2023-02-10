c51f864ea3b5503e0e95bcbe58038d31d8601a00
Curtis Turk <curtis.c.turk@outlook.com>
2023-02-10 16:11:01 +0000

    Basic tests passing

--- JavaScript/validParentheses/validParenthesis.js
+++ JavaScript/validParentheses/validParenthesis.js
@@ -1,5 +1,17 @@
 function validParentheses(parens) {
-  return true;
+  let lCount = 0;
+  let rCount = 0;
+  const pArray = parens.split("");
+
+  for (let i = 0; i < pArray.length; i++) {
+    if (pArray[i] === "(") {
+      lCount++;
+    } else if (pArray[i] === ")") {
+      rCount++;
+    }
+  }
+
+  return lCount === rCount;
 }
 
 module.exports = validParentheses;
--- JavaScript/validParentheses/validParenthesis.test.js
+++ JavaScript/validParentheses/validParenthesis.test.js
@@ -7,3 +7,10 @@ test("Returns true with a ()", () => {
 test("Returns false with a (", () => {
   expect(validParentheses("(")).toEqual(false);
 });
+
+// Code wars basic tests
+test(`values: "("`, () => expect(validParentheses("(")).toEqual(false));
+test(`values: ")"`, () => expect(validParentheses(")")).toEqual(false));
+test(`values: ""`, () => expect(validParentheses("")).toEqual(true));
+test(`values: "()"`, () => expect(validParentheses("()")).toEqual(true));
+test(`values: "())"`, () => expect(validParentheses("())")).toEqual(false));
