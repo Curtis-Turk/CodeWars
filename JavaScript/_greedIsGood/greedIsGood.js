aa725dfbf9358cc04485d57201ca4030d95d5db1
Curtis Turk <curtis.c.turk@outlook.com>
2023-02-16 18:38:17 +0000

    refactored count object function

--- JavaScript/_greedIsGood/greedIsGood.js
+++ JavaScript/_greedIsGood/greedIsGood.js
@@ -1,4 +1,15 @@
 function score(dice) {
+  const countObj = buildCountObj(dice);
+
+  if (countObj["1"] === 5) {
+    return 1200;
+  } else if (countObj["1"] === 1) {
+    return 100;
+  }
+  return 0;
+}
+
+function buildCountObj(dice) {
   let countObj = {};
   for (let i = 0; i < dice.length; i++) {
     if (!countObj[dice[i]]) {
@@ -7,13 +18,7 @@ function score(dice) {
       countObj[dice[i]]++;
     }
   }
-
-  if (countObj["1"] === 5) {
-    return 1200;
-  } else if (countObj["1"] === 1) {
-    return 100;
-  }
-  return 0;
+  return countObj;
 }
 
 module.exports = score;
