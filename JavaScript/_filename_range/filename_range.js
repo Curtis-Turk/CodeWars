d88af540e42353a65ba6dc5da00c027ac3e26358
Curtis Turk <curtis.c.turk@outlook.com>
2023-05-16 15:41:25 +0100

    tests passing

--- JavaScript/_filename_range/filename_range.js
+++ JavaScript/_filename_range/filename_range.js
@@ -2,11 +2,12 @@ const fileNameRangeSelect = (filename) => {
   const fileArr = filename.split("");
   let range = [0, 0];
   for (let i = 0; i < fileArr.length; i++) {
+    if (checkForTest(fileArr.slice(i, i + 4))) i += 4;
+
     const char = fileArr[i];
     range[1]++;
-    if (char === "/") {
-      range[0] = i + 1;
-    }
+
+    if (char === "/") range[0] = i + 1;
     if (char === "." || char === "_") {
       range[1]--;
       break;
@@ -14,6 +15,10 @@ const fileNameRangeSelect = (filename) => {
   }
   console.log(filename, range);
   return range;
+};
+
+const checkForTest = (fourCharSubStr) => {
+  return fourCharSubStr.join("").toLowerCase() === "test";
 };
 
 module.exports = fileNameRangeSelect;
